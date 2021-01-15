d5423d665f87169d82168412c3776a00975718bc
xarantolus <xarantolus@protonmail.com>
2021-01-15 20:35:37 +0100

    Many improvements & debug mode

--- main.go
+++ main.go
@@ -2,22 +2,31 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"flag"
+	"log"
 	"os"
 	"os/exec"
+	"sync"
 	"time"
 
 	"github.com/xarantolus/backtap/input"
 	"github.com/xarantolus/backtap/listener"
 )
 
+var debugMode bool
+
 func main() {
-	fmt.Println("Start")
+	// Output debug timestamps with milliseconds
+	log.SetFlags(log.Flags() | log.Lmicroseconds)
+	flag.BoolVar(&debugMode, "debug", false, "Enable debug mode")
+	flag.Parse()
 
 	// we use this context to make sure all processes we start etc.
 	// are closed/killed correctly in case something goes wrong
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
+	debug("starting listener")
 
 	// Set up the stream we're listening to
 	fingerEvents, err := listener.LogCat(ctx)
@@ -28,6 +37,7 @@ func main() {
 	// Open all device files we want to write to
 
 	// Vibrator: When we write a number as string, the devices vibrates for that amount in milliseconds
+	debug("opening vibrator device")
 	vibratorDevice, err := os.OpenFile("/sys/devices/virtual/timed_output/vibrator/enable", os.O_WRONLY, os.ModeDevice)
 	if err != nil {
 		panic("cannot open vibrator device: " + err.Error())
@@ -35,6 +45,7 @@ func main() {
 	defer vibratorDevice.Close()
 
 	// Key: This input allows us to simulate a power button press
+	debug("opening key device")
 	keyDevice, err := os.OpenFile("/dev/input/event0", os.O_WRONLY, os.ModeDevice)
 	if err != nil {
 		panic("opening touch device input file: " + err.Error())
@@ -44,85 +55,110 @@ func main() {
 	// Touch: This input allows us to tap on the screen
 	// This also works when the lockscreen is shown, so we should
 	// be rather careful not to call emergency services accidentally
+	debug("opening touchscreen device")
 	touchDevice, err := os.OpenFile("/dev/input/event1", os.O_WRONLY, os.ModeDevice)
 	if err != nil {
 		panic("opening touch device input file: " + err.Error())
 	}
 	defer touchDevice.Close()
 
+	// The last time a certain action was performed
 	var (
 		lastTapTime   time.Time
 		lastPowerTime time.Time
 	)
 
+	// This is for the "BACK" command. Since it needs to wait until the finger is on the sensor
+	// for a few moments, its state management is a bit more complicated
 	var (
-		buttonHolder      = make(chan bool)
+		buttonAbort       = make(chan bool)
 		backButtonPressed = false
+		backButtonLock    sync.Mutex
 	)
+
+	debug("start processing events")
 
 	// OK, now we process all events that come up
 	for event := range fingerEvents {
 		switch event {
 		case listener.FINGER_UP:
+			debug("FINGER_UP fired")
+
 			// The finger was just lifted from the sensor
 
 			// This sends - if possible - an abort signal to the button holding goroutine below
 			select {
-			case buttonHolder <- false:
+			case buttonAbort <- false:
 			default:
 			}
 
+			backButtonLock.Lock()
 			if backButtonPressed {
+				debug("Not clicking because we just ran the FORWARD command")
 				backButtonPressed = false
+				backButtonLock.Unlock()
 			} else {
+				backButtonLock.Unlock()
+
 				if time.Since(lastTapTime) < 350*time.Millisecond {
 					// This prevents pressing the power button twice quickly, which would open the default camera
-					if time.Since(lastPowerTime) > 350*time.Millisecond {
+					if time.Since(lastPowerTime) > 750*time.Millisecond {
+						debug("Running SCREENOFF command")
 
-						fmt.Println("Power")
 						err = input.PressPowerButton(keyDevice)
 						if err != nil {
 							panic("pressing power button: " + err.Error())
 						}
-						lastPowerTime = time.Now()
-						err := exec.Command("input", "keyevent", "KEYCODE_BACK").Run()
-						if err != nil {
-							panic("pressing back button: " + err.Error())
-						}
+						debug("Finished SCREENOFF command")
 
+						lastPowerTime = time.Now()
+					} else {
+						debug("Skipped SCREENOFF command as the screen is currently turning off")
 					}
 				} else {
 					// Top left coordinates
 
-					fmt.Println("Topleft")
+					debug("Running TOUCH command")
 					err = input.TouchUpDown(touchDevice, 35, 105)
 					if err != nil {
 						panic("running touch command: " + err.Error())
 					}
+					debug("Finished TOUCH command")
 				}
 				lastTapTime = time.Now()
 			}
 		case listener.FINGER_DOWN:
+			debug("FINGER_DOWN fired")
 			go func() {
 				select {
-				case <-buttonHolder:
+				case <-buttonAbort:
+					debug("aborted FORWARD command")
 					return
 				case <-time.After(250 * time.Millisecond):
+					debug("Running FORWARD command")
 
-					fmt.Println("Back")
+					backButtonLock.Lock()
 					backButtonPressed = true
+					backButtonLock.Unlock()
 
-					err := exec.Command("input", "keyevent", "KEYCODE_BACK").Run()
+					err := exec.Command("input", "keyevent", "KEYCODE_FORWARD").Run()
 					if err != nil {
-						panic("pressing back button: " + err.Error())
+						panic("pressing forward button: " + err.Error())
 					}
 					err = input.Vibrate(vibratorDevice, 50)
 					if err != nil {
 						panic("cannot vibrate: " + err.Error())
 					}
+					debug("Finished FORWARD command")
 				}
 			}()
 
 		}
+	}
+}
+
+func debug(s ...interface{}) {
+	if debugMode {
+		log.Println(s...)
 	}
 }
